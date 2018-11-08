aac1e1dcce83386bec7d65ed9d5db37e3e25bac9
frostmoroz83@yandex.ru <frostmoroz83@yandex.ru>
2018-11-08 22:12:14 +0400

    7777

---
+++ src/Dropdown.js
@@ -0,0 +1,30 @@
+import React, { Component} from 'react';
+
+class Dropdown extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isOpened: false};
+
+
+  }
+  toggleState() {
+    this.setState({ isOpened: !this.state.isOpened});
+
+  }
+
+  render() {
+    let dropdownText;
+    if (this.state.isOpened) {
+      dropdownText = <div>Here is what is shown in dropdown</div>
+    }
+    return (
+    <div onClick={this.toggleState.bind(this)}>
+    Its Dropdown
+      {dropdownText}
+    </div>
+    );
+  }
+
+}
+
+export default Dropdown;
